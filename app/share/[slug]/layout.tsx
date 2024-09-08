d32cefa5d047fc4e685277ceafab3324dc6c5e44
Troy Magennis <dev@predictabilityatscale.com>
2024-09-07 18:02:14 -0700

    testing share page og:

--- app/share/[slug]/layout.tsx
+++ app/share/[slug]/layout.tsx
@@ -1,23 +1,7 @@
-import type { Metadata } from "next";
+
 import "../../globals.css";
-import Head from "next/head";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
-export const metadata: Metadata = {
-  metadataBase: new URL('https://shark-puddle.com'),
-  title: "Shark Puddle",
-  description:
-    "Shark Puddle business idea pitch platform (demo app for LLMAsAService.io)",
-    openGraph: {
-      title: "Shark Puddle",
-      description:
-        "Shark Puddle business idea pitch platform (demo app for LLMAsAService.io)",
-      type: "website",
-      url: "https://shark-puddle.com",
-      images: 
-        "/sharkpuddlescene.png"
-    },
-};
 
 export default function RootLayout({
   children,
--- app/share/[slug]/page.tsx
+++ app/share/[slug]/page.tsx
@@ -9,7 +9,23 @@ import outputs from "@/amplify_outputs.json";
 import Head from "next/head";
 import type { Metadata, ResolvingMetadata } from 'next'
 
-
+/*
+export const metadata: Metadata = {
+  metadataBase: new URL('https://shark-puddle.com'),
+  title: "Shark Puddle",
+  description:
+    "Shark Puddle business idea pitch platform (demo app for LLMAsAService.io)",
+    openGraph: {
+      title: "Shark Puddle",
+      description:
+        "Shark Puddle business idea pitch platform (demo app for LLMAsAService.io)",
+      type: "website",
+      url: "https://shark-puddle.com",
+      images: 
+        "/sharkpuddlescene.png"
+    },
+};
+*/
 
 const Page: React.FC<{ params: { slug: string } }> = ({ params }) => {
   Amplify.configure(outputs);
@@ -82,6 +98,15 @@ Built using LLMAsAService.io. [Learn more](https://llmasaservice.io)`;
 
   return (
     <>
+          <Head>
+        <title>Shark Puddle</title>
+        <meta name="description" content="xxx" />
+        <meta property="og:title" content="xxx" />
+        <meta property="og:description" content="xxxlatform (demo app for LLMAsAService.io)" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={"//shark-puddle.com/share/" + idea?.id} />
+        <meta property="og:image" content="https://shark-puddle.com/sharkpuddlescene.png" />
+      </Head>
     <div className="p-1 bg-black min-h-screen w-full">
       <div className="flex flex-col items-center mt-8">
         <div className="bg-gray-800 p-8 rounded shadow-md w-full max-w-6xl">
